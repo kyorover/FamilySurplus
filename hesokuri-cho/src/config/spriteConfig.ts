e64a8fe6a4bf4f406b287884b8cbeccc30e28461
kyorover <kyo.futsa101@gmail.com>
2026-04-14 23:02:36 +0900

    画像調整

--- hesokuri-cho/src/config/spriteConfig.ts
+++ hesokuri-cho/src/config/spriteConfig.ts
@@ -1,7 +1,7 @@
 // src/config/spriteConfig.ts
 import { ImageSourcePropType } from 'react-native';
 
-export type SpriteSourceId = 'tile1' | 'tile2' | 'tile3' | 'tile4' | 'tile5' | 'tile6' | 'tile7' | 'tree1' | 'item1' | 'item2' | 'item3' | 'item4' | 'item5' | 'arrow' | 'chara' | 'levelup' | 'wp1' | 'wp2' | 'wp3' | 'wp4' | 'wp5' | 'wp7' | 'wp8'; // ▼ 追加: 壁紙のソースID
+export type SpriteSourceId = 'tile1' | 'tile2' | 'tile3' | 'tile4' | 'tile5' | 'tile6' | 'tile7' | 'tree1' | 'item1' | 'item2' | 'item3' | 'item4' | 'item5' | 'arrow' | 'chara1' | 'chara2' | 'levelup' | 'wp1' | 'wp2' | 'wp3' | 'wp4' | 'wp5' | 'wp7' | 'wp8'; // ▼ 追加: 壁紙のソースID
 
 export interface SpriteDefinition {
   sourceId: SpriteSourceId;
@@ -37,7 +37,8 @@ export const IMAGE_SOURCES: Record<SpriteSourceId, ImageSourcePropType> = {
   tile6: require('../../assets/images/garden/tile_6.png'),
   tile7: require('../../assets/images/garden/tile_7.png'),
   arrow: require('../../assets/images/garden/arrow.png'),
-  chara: require('../../assets/images/garden/chara.png'),
+  chara1: require('../../assets/images/garden/chara1.png'),
+  chara2: require('../../assets/images/garden/chara2.png'),
   levelup: require('../../assets/images/garden/levelup.png'),
   wp1: require('../../assets/images/garden/bg_1.png'),
   wp2: require('../../assets/images/garden/bg_2.png'),
@@ -243,24 +244,54 @@ export const SPRITE_CONFIG: Record<string, SpriteDefinition> = {
     baseScale: 0.8, isAnimated: true, animationSpeed: 350, maxQuantity: 1,
   },
   'CR-200': { // 子供(男の子)
-    sourceId: 'chara', originalWidth: 455, originalHeight: 384, startX: 16, startY: 157,
-    frameWidth: 50, frameHeight: 76, frameCount: 2, frameSpacingX: 0, offsetX: 2, offsetY: 50,
-    baseScale: 0.4, isAnimated: true, animationSpeed: 500, maxQuantity: 1,
+    sourceId: 'chara2', originalWidth: 459, originalHeight: 396, startX: 0, startY: 3,
+    frameWidth: 153, frameHeight: 129, frameCount: 3, frameSpacingX: 0, offsetX: 0, offsetY: 48,
+    baseScale: 0.7, isAnimated: true, animationSpeed: 500, maxQuantity: 1,
   },
   'CR-201': { // 子供(女の子)
     sourceId: 'item5', originalWidth: 648, originalHeight: 385, startX: 0, startY: 316,
-    frameWidth: 80, frameHeight: 64, frameCount: 4, frameSpacingX: 0, offsetX: 0, offsetY: 44,
+    frameWidth: 80, frameHeight: 64, frameCount: 4, frameSpacingX: 0, offsetX: 0, offsetY: 48,
     baseScale: 0.7, isAnimated: true, animationSpeed: 500, maxQuantity: 1,
   },
-  'CR-210': { // 大人(女)
-    sourceId: 'chara', originalWidth: 455, originalHeight: 384, startX: 0, startY: 234,
-    frameWidth: 58, frameHeight: 76, frameCount: 2, frameSpacingX: 0, offsetX: 0, offsetY: 52,
-    baseScale: 0.6, isAnimated: true, animationSpeed: 2000, maxQuantity: 1,
+  'CR-202': { // パパ
+    sourceId: 'chara2', originalWidth: 459, originalHeight: 396, startX: 0, startY: 133,
+    frameWidth: 153, frameHeight: 132, frameCount: 3, frameSpacingX: 0, offsetX: 0, offsetY: 48,
+    baseScale: 0.7, isAnimated: true, animationSpeed: 600, maxQuantity: 1,
   },
-  'CR-211': { // 大人(男)
-    sourceId: 'chara', originalWidth: 455, originalHeight: 384, startX: 0, startY: 310,
-    frameWidth: 60, frameHeight: 76, frameCount: 2, frameSpacingX: 0, offsetX: 0, offsetY: 46,
-    baseScale: 0.6, isAnimated: true, animationSpeed: 2000, maxQuantity: 1,
+  'CR-203': { // ママ
+    sourceId: 'chara2', originalWidth: 459, originalHeight: 396, startX: 0, startY: 266,
+    frameWidth: 153, frameHeight: 132, frameCount: 3, frameSpacingX: 0, offsetX: 0, offsetY: 48,
+    baseScale: 0.7, isAnimated: true, animationSpeed: 600, maxQuantity: 1,
+  },
+  'CR-204': { // 社会人(男)
+    sourceId: 'chara1', originalWidth: 459, originalHeight: 799, startX: 0, startY: 137,
+    frameWidth: 153, frameHeight: 133, frameCount: 2, frameSpacingX: 0, offsetX: 0, offsetY: 46,
+    baseScale: 0.7, isAnimated: true, animationSpeed: 2000, maxQuantity: 1,
+  },
+  'CR-205': { // 社会人(女)
+    sourceId: 'chara1', originalWidth: 459, originalHeight: 799, startX: 0, startY: 3,
+    frameWidth: 153, frameHeight: 130, frameCount: 2, frameSpacingX: 0, offsetX: 0, offsetY: 46,
+    baseScale: 0.7, isAnimated: true, animationSpeed: 2000, maxQuantity: 1,
+  },
+  'CR-206': { // 大学生(男)
+    sourceId: 'chara1', originalWidth: 459, originalHeight: 799, startX: 0, startY: 410,
+    frameWidth: 153, frameHeight: 133, frameCount: 3, frameSpacingX: 0, offsetX: 0, offsetY: 46,
+    baseScale: 0.7, isAnimated: true, animationSpeed: 2000, maxQuantity: 1,
+  },
+  'CR-207': { // 大学生(女)
+    sourceId: 'chara1', originalWidth: 459, originalHeight: 799, startX: 0, startY: 270,
+    frameWidth: 153, frameHeight: 133, frameCount: 3, frameSpacingX: 0, offsetX: 0, offsetY: 46,
+    baseScale: 0.7, isAnimated: true, animationSpeed: 2000, maxQuantity: 1,
+  },
+  'CR-208': { // おじいちゃん
+    sourceId: 'chara1', originalWidth: 459, originalHeight: 799, startX: 0, startY: 533,
+    frameWidth: 153, frameHeight: 133, frameCount: 2, frameSpacingX: 0, offsetX: 0, offsetY: 46,
+    baseScale: 0.7, isAnimated: true, animationSpeed: 2000, maxQuantity: 1,
+  },
+  'CR-209': { // おばあちゃん
+    sourceId: 'chara1', originalWidth: 459, originalHeight: 799, startX: 0, startY: 666,
+    frameWidth: 153, frameHeight: 133, frameCount: 2, frameSpacingX: 0, offsetX: 0, offsetY: 46,
+    baseScale: 0.7, isAnimated: true, animationSpeed: 2000, maxQuantity: 1,
   },
   'WP-01': {
     sourceId: 'wp1', originalWidth: 976, originalHeight: 1056, startX: 0, startY: 0,
